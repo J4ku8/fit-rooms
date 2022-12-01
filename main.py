8f188fb35967ed7e93609723a7d369ec62bae551
Jakub Tichý <tichyj15@fit.cvut.cz>
2022-12-01 11:09:42 +0100

    checkboxes + layout

--- main.py
+++ main.py
@@ -14,13 +14,34 @@ uniqYears = list(dict.fromkeys([date.year for date in dates_list]))
 
 app = Dash(__name__)
 
-app.layout = html.Div([
-    html.Br(),
-    # TODO: side menu with dropwodn and checkboxes
-    dcc.Dropdown(uniqYears, uniqYears[0], id='year-dropdown'),
-    dcc.Graph(id='graph-with-dropdown'),
-    dcc.Graph(id='pie'),
-    dcc.Graph(id='sankey'),
+app.layout = html.Div(style={'display': 'grid',
+                             "grid-template-columns": "1fr 3fr",
+                             "grid-template-rows": "1fr",
+                             "grid-column-gap": "0px",
+                             "grid-row-gap": "0px",
+                             }, children=[
+    html.Div(style={
+        "background-color": "#a2a6a3",
+    }, children=[
+        html.Div(style={"margin": "5rem 2rem"}, children=[
+            dcc.Dropdown(uniqYears, uniqYears[0], id='year-dropdown', clearable=False),
+            dcc.Checklist(
+                id="line-checklist",
+                options=[
+                    {'label': 'Apple', 'value': 'AAPL'},
+                    {'label': 'Google', 'value': 'GOOG'},
+                    {'label': 'Facebook', 'value': 'FB'},
+                ],
+                value=['AAPL'],
+                inline=False
+            )
+        ])
+    ]),
+    html.Div([
+        dcc.Graph(id='graph-with-dropdown'),
+        dcc.Graph(id='pie'),
+        dcc.Graph(id='sankey'),
+    ])
 ])
 
 
@@ -28,8 +49,9 @@ app.layout = html.Div([
     Output('graph-with-dropdown', 'figure'),
     Output('pie', 'figure'),
     Output('sankey', 'figure'),
-    Input('year-dropdown', 'value'))
-def update_figure(selected_year):
+    Input('year-dropdown', 'value'),
+    Input('line-checklist', 'value'))
+def update_figure(selected_year, selected):
     data = df[df['year'] == selected_year]
 
     fig = go.Figure()
@@ -45,12 +67,25 @@ def update_figure(selected_year):
     #                      marker_color='rgb(26, 118, 255)'
     #                      ))
     #
-    fig.add_trace(go.Bar( x=data["date"],
-                         y=data["NFLX"]/10,
+    fig.add_trace(go.Bar(x=data["date"],
+                         y=data["NFLX"] / 10,
                          marker_color='rgba(124, 189, 255, 0.5)',
-                          name="NTFLX devided by 10"
+                         name="NTFLX devided by 10"
                          ))
 
+    names = {'AAPL': "Apple", 'GOOG': "Google", 'FB': "Facebook"}
+    colors = {'AAPL': "#2a2b2a", 'GOOG': "#F4B400", 'FB': "#4267B2"}
+    for item in selected:
+        fig.add_trace(go.Scatter(x=data["date"], y=data[item],
+                                 mode='lines', name=names.get(item) ))
+
+    labels = ["AAPL", "GOOG", "FB"]
+
+    values = [data['AAPL'].sum(), data['GOOG'].sum(), data['FB'].sum()]
+    fig2.add_trace(go.Pie(values=values, labels=labels, hole=.3))
+
+
+    # TODO: connect sankey to dropdown and checkboxes
     sankey = go.Sankey(
         arrangement="snap",
         node=dict(
@@ -65,17 +100,6 @@ def update_figure(selected_year):
             value=[1, 2]))
 
     fig3.add_trace(sankey)
-
-    fig.add_trace(go.Scatter(x=data["date"], y=data["AAPL"],
-                             mode='lines', name='Apple',))
-    fig.add_trace(go.Scatter(x=data["date"], y=data["GOOG"],
-                             mode='lines', name='Google',))
-    fig.add_trace(go.Scatter(x=data["date"], y=data["FB"],
-                             mode='lines', name='FB',))
-
-    labels = ['AAPL', 'GOOG', 'FB']
-    values = [data['AAPL'].sum(), data['GOOG'].sum(), data['FB'].sum()]
-    fig2.add_trace(go.Pie(values=values, labels=labels, hole=.3))
 
     fig.update_layout(transition_duration=500)
 
