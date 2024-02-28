6b3af23230515ce300d6bcec8796f9c5242a07ab
J4ku8 <41336972+J4ku8@users.noreply.github.com>
2024-02-28 21:42:40 +0100

    todo: fix semester fethcing

--- microservices/synchronization/src/app.ts
+++ microservices/synchronization/src/app.ts
@@ -4,12 +4,10 @@ import bodyParser from "body-parser";
 import initCrons from "./midleware/cron";
 import {KosApiClient} from "./controller/cvut-kos/KosClient";
 import connect from "./db";
-import {TokenManager} from "./midleware/auth/TokenManager";
 import MicrosoftClient from "./controller/ms-teams/MicrosoftClient";
 
 import {parseCourseEvents, parseExams, parseParrallels} from "./midleware/synchronization";
 import {WEEKS_OF_LECTURES} from "./utils/constants";
-import microsoftClient from "./controller/ms-teams/MicrosoftClient";
 const app: Express = express();
 
 
@@ -20,23 +18,23 @@ const microsoft_client = new MicrosoftClient()
 const kos_client = new KosApiClient();
 (async () => {
     const db = await connect()
-    const microsoftRooms = await microsoft_client.listRooms()
     const semester = await kos_client.getSemester()
-    // const courseEvents = await kos_client.getCourseEvent(semester.name);
-    // const exams = await kos_client.getExams(semester.name)
-    // const parallels = await kos_client.getParallels(semester?.name);
-    // const endOfLectures = new Date(new Date(semester.from).setDate(semester.from.getDate() + (WEEKS_OF_LECTURES * 7) + 5 ))
-    // const microsoftParralels = await parseParrallels({ semesterStart: semester.from, semesterEnd: endOfLectures, data: parallels})
-    // const microsoftExams = await parseExams(exams)
-    // const microsoftCourseEvents = await parseCourseEvents(courseEvents)
-
-    // await microsoft_client.createEvent({ roomEmail: events[0].attendees[0].emailAddress.address, event: events[0]})
+    const courseEvents = await kos_client.getCourseEvent(semester.name);
+    const exams = await kos_client.getExams(semester.name)
+    const parallels = await kos_client.getParallels(semester?.name);
+    const endOfLectures = new Date(new Date(semester.from).setDate(semester.from.getDate() + (WEEKS_OF_LECTURES * 7) + 5 ))
+    const microsoftParralels = await parseParrallels({ semesterStart: semester.from, semesterEnd: endOfLectures, data: parallels})
+    const microsoftExams = await parseExams(exams)
+    const microsoftCourseEvents = await parseCourseEvents(courseEvents)
+    const allEvents = [...microsoftParralels, ...microsoftExams, ...microsoftCourseEvents]
+    allEvents.forEach(async (event) => {
+        await microsoft_client.createEvent({ roomEmail: event.attendees[0].emailAddress.address, event: event})
+    })
     // // @ts-ignore
     // roomEvents?.map(item => {
     //     microsoft_client.deleteEvent({ roomEmail: events[0].attendees[0].emailAddress.address, eventId: item.id})
     // })
-
-    // initCrons(kos_client, microsoft_client)
+    initCrons(kos_client, microsoft_client)
 })();
 
 export default app;
